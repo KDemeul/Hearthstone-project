24eb32a951d412ba487ba6c1f273bae37a225eff
KDemeul <kilian.demeule@gmail.com>
2014-12-03 20:36:45 +0100

    [Search Engine] Working version of search engine

--- test/search-results.php
+++ test/search-results.php
@@ -1,11 +1,11 @@
 <?php
 // Function createListElement
-function createListElement($card)
+function createListElement($card,$active)
 {
-	echo '<tr>';
+	echo '<tr' . ($active ? ' class="info"' : '') . '>';
 	echo '<td><form action="index.php" method="get">';
 	echo '<input type="hidden" name="q" value="' . $_GET['q'] . '">';
-	echo '<button type="submit" name="id" value="' . $card['id'] . '">' . $card['name'] .'</button>';
+	echo '<button type="submit" name="id" value="' . $card['id'] . '" class="flatButton">' . $card['name'] .'</button>';
 	echo '</form></td>';
 	echo '<td>' . $card['type'] . '</td>' . "\n";
 	echo '<td>' . $card['text'] . '</td>' . "\n";
@@ -44,26 +44,26 @@ if(isset($_GET['id'])){
 $card_query = $bdd->prepare('SELECT * FROM allset WHERE id = :key');
 
 $querys = array(
-	'id' => $bdd->prepare('SELECT * FROM allset WHERE LOCATE(:key,id)>0'),
-	'name' => $bdd->prepare('SELECT * FROM allset WHERE LOCATE(:key,name)>0'),
-	'type' => $bdd->prepare('SELECT * FROM allset WHERE LOCATE(:key,type)>0'),
-	'text' => $bdd->prepare('SELECT * FROM allset WHERE LOCATE(:key,text)>0'),
-	'playerClass' => $bdd->prepare('SELECT * FROM allset WHERE LOCATE(:key,playerClass)>0'),
-	'mechanics' => $bdd->prepare('SELECT * FROM allset WHERE LOCATE(:key,mechanics)>0'),
-	'faction' => $bdd->prepare('SELECT * FROM allset WHERE LOCATE(:key,faction)>0'),
-	'rarity' => $bdd->prepare('SELECT * FROM allset WHERE LOCATE(:key,rarity)>0'),
-	'health' => $bdd->prepare('SELECT * FROM allset WHERE LOCATE(:key,health)>0'),
-	'collectible' => $bdd->prepare('SELECT * FROM allset WHERE LOCATE(:key,collectible)>0'),
-	'cost' => $bdd->prepare('SELECT * FROM allset WHERE LOCATE(:key,cost)>0'),
-	'attack' => $bdd->prepare('SELECT * FROM allset WHERE LOCATE(:key,attack)>0'),
-	'flavor' => $bdd->prepare('SELECT * FROM allset WHERE LOCATE(:key,flavor)>0'),
-	'artist' => $bdd->prepare('SELECT * FROM allset WHERE LOCATE(:key,artist)>0'),
-	'howToGetGold' => $bdd->prepare('SELECT * FROM allset WHERE LOCATE(:key,howToGetGold)>0'),
-	'howToGet' => $bdd->prepare('SELECT * FROM allset WHERE LOCATE(:key,howToGet)>0'),
-	'inPlayText' => $bdd->prepare('SELECT * FROM allset WHERE LOCATE(:key,inPlayText)>0'),
-	'race' => $bdd->prepare('SELECT * FROM allset WHERE LOCATE(:key,race)>0'),
-	'durability' => $bdd->prepare('SELECT * FROM allset WHERE LOCATE(:key,durability)>0'),
-	'elite' => $bdd->prepare('SELECT * FROM allset WHERE LOCATE(:key,elite)>0')
+	'id' => $bdd->prepare('SELECT id FROM allset WHERE LOCATE(:key,id)>0'),
+	'name' => $bdd->prepare('SELECT id FROM allset WHERE LOCATE(:key,name)>0'),
+	'type' => $bdd->prepare('SELECT id FROM allset WHERE LOCATE(:key,type)>0'),
+	'text' => $bdd->prepare('SELECT id FROM allset WHERE LOCATE(:key,text)>0'),
+	'playerClass' => $bdd->prepare('SELECT id FROM allset WHERE LOCATE(:key,playerClass)>0'),
+	'mechanics' => $bdd->prepare('SELECT id FROM allset WHERE LOCATE(:key,mechanics)>0'),
+	'faction' => $bdd->prepare('SELECT id FROM allset WHERE LOCATE(:key,faction)>0'),
+	'rarity' => $bdd->prepare('SELECT id FROM allset WHERE LOCATE(:key,rarity)>0'),
+	'health' => $bdd->prepare('SELECT id FROM allset WHERE LOCATE(:key,health)>0'),
+	'collectible' => $bdd->prepare('SELECT id FROM allset WHERE LOCATE(:key,collectible)>0'),
+	'cost' => $bdd->prepare('SELECT id FROM allset WHERE LOCATE(:key,cost)>0'),
+	'attack' => $bdd->prepare('SELECT id FROM allset WHERE LOCATE(:key,attack)>0'),
+	'flavor' => $bdd->prepare('SELECT id FROM allset WHERE LOCATE(:key,flavor)>0'),
+	'artist' => $bdd->prepare('SELECT id FROM allset WHERE LOCATE(:key,artist)>0'),
+	'howToGetGold' => $bdd->prepare('SELECT id FROM allset WHERE LOCATE(:key,howToGetGold)>0'),
+	'howToGet' => $bdd->prepare('SELECT id FROM allset WHERE LOCATE(:key,howToGet)>0'),
+	'inPlayText' => $bdd->prepare('SELECT id FROM allset WHERE LOCATE(:key,inPlayText)>0'),
+	'race' => $bdd->prepare('SELECT id FROM allset WHERE LOCATE(:key,race)>0'),
+	'durability' => $bdd->prepare('SELECT id FROM allset WHERE LOCATE(:key,durability)>0'),
+	'elite' => $bdd->prepare('SELECT id FROM allset WHERE LOCATE(:key,elite)>0')
 	);
 
 	?>
@@ -77,26 +77,36 @@ $querys = array(
 			foreach ($querys as $query) {
 				$query->execute(array('key' => $key_words[0]));
 				while($card = $query->fetch()){
-					$results[$card['id']] = -1;
+					$results[$card['id']] = array() ;
 				}
 			}
+			
 			foreach ($key_words as $key) {
 				foreach ($querys as $query) {
 					$query->execute(array('key' => $key));
 					while($card = $query->fetch()){
 						if(array_key_exists($card['id'], $results)){
-							$results[$card['id']] += 1;
+							$results[$card['id']] = array_merge($results[$card['id']],array($key));
 						}
 					}
 				}
 			}
+			// Clean results
+			foreach (array_keys($results) as $card_id) {
+				$results[$card_id] = array_unique($results[$card_id]);
+			}
+
 			foreach(array_keys($results) as $card_id)
 			{
-				if($results[$card_id] >= sizeof($key_words)){	
+				if(sizeof($results[$card_id]) == sizeof($key_words)){	
 					$card_query->execute(array('key' => $card_id));
-					while($card = $card_query->fetch())
-						createListElement($card);
-
+					while($card = $card_query->fetch()){
+						if(isset($_GET['id']) and $card['id'] == $_GET['id']){
+							createListElement($card,true);
+						} else {
+							createListElement($card,false);
+						}
+					}
 				}
 			}
 			?>
